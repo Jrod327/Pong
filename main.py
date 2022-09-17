e93217d91d1e967eeff1c465f126102dd5786c11
Jrod327 <113052919+Jrod327@users.noreply.github.com>
2022-09-16 21:12:02 -0400

    Final Version

--- main.py
+++ main.py
@@ -27,26 +27,26 @@ screen.onkey(paddle_right.move_down, "Down")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
 
+    # Collision with ceiling/floor
     if ball.ycor() > 280 or ball.ycor() < -280:
         ball.bounce_y()
 
+    # Collision with paddle
     if ball.distance(paddle_right) < 50 and ball.xcor() > 340:
         ball.bounce_x()
     elif ball.distance(paddle_left) < 50 and ball.xcor() < -340:
         ball.bounce_x()
 
-"""
-scoring logic - if ball xcor > 400 or < -400, add point to other paddle score. Reset ball to beginning
-"""
-
+    # Missed ball, one of the players scores
+    if ball.xcor() > 400:
+        scoreboard.increase_score_left()
+        ball.reset_ball()
+    elif ball.xcor() < -400:
+        scoreboard.increase_score_right()
+        ball.reset_ball()
 
 screen.exitonclick()
-# TODO 4 Create the ball and make it move
-# TODO 5 Detect collision with wall and bounce
-# TODO 6 Detect collision with a paddle
-# TODO 7 Increase opposite paddle score when paddle misses
-
--- scoreboard.py
+++ scoreboard.py
@@ -10,14 +10,6 @@ class Scoreboard(Turtle):
         self.speed(0)
         self.penup()
         self.goto(0, -310)
-        self.pencolor("white")
-        self.setheading(90)
-        self.pensize(5)
-        for i in range(30):
-            self.pendown()
-            self.forward(5)
-            self.penup()
-            self.forward(25)
         self.hideturtle()
         self.color("white")
         self.score_left = 0
@@ -30,15 +22,12 @@ class Scoreboard(Turtle):
         self.goto(100, 195)
         self.write(self.score_right, align=ALIGNMENT, font=FONT)
 
-    def increase_score(self, paddle_side):
+    def increase_score_left(self):
         self.clear()
-        paddle_side += 1
+        self.score_left += 1
         self.update_scoreboard()
 
-    def game_over(self):
-        self.hideturtle()
-        self.penup()
-        self.goto(0, 0)
-        self.write(f"GAME OVER", align=ALIGNMENT, font=FONT)
-        self.goto(0, -60)
-        self.write(f"Press 'R' to restart or 'Q' to quit.", align=ALIGNMENT, font=("Courier", 12, "normal"))
+    def increase_score_right(self):
+        self.clear()
+        self.score_right += 1
+        self.update_scoreboard()
